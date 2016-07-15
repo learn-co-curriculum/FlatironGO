9e964905d4495f831def22c1731694ae785f0cf6
joelconnects <joelconnects@gmail.com>
2016-07-15 19:42:46 -0400

    added Firebase platform

--- FlatironGo/AppDelegate.swift
+++ FlatironGo/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,7 +16,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        // Firebase Initialization
+        FIRApp.configure()
+        
         return true
     }
 
