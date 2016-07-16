515b2fbc0f56f253601c5adc16f6ca6133f9906e
HaarisMuneer <hom9@cornell.edu>
2016-07-15 22:34:24 -0400

    broken - will fix

--- FlatironGo/Treasure.swift
+++ FlatironGo/Treasure.swift
@@ -15,8 +15,10 @@ struct Treasure: CustomStringConvertible {
     var description: String { return makeDescription() }
     var image: UIImage
     
-    init(location: GPSLocation, image: UIImage) {
+    init(location: GPSLocation, imageURLString: String) {
         self.location = location
+        guard let imageURL = NSURL(string: imageURLString) else { print("Error getting the image. Fix this later yo"); return }
+        let image = UIImage()
         self.image = image
         createItem()
     }
