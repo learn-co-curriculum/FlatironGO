56a13d2be86983d5b7207797fbb8d1af8141bec6
HaarisMuneer <hom9@cornell.edu>
2016-07-15 21:57:20 -0400

    treasure struct created

--- FlatironGo/Treasure.swift
+++ FlatironGo/Treasure.swift
@@ -11,17 +11,23 @@ import UIKit
 
 struct Treasure: CustomStringConvertible {
     let location: GPSLocation
-    let item = CALayer()
+    var item = CALayer()
     var description: String { return makeDescription() }
+    var image: UIImage
     
-    init(location: GPSLocation) {
+    init(location: GPSLocation, image: UIImage) {
         self.location = location
+        self.image = image
+        createItem()
     }
     
     func makeDescription() -> String {
         return "\(location.latitude), \(location.longitude)"
     }
     
+    mutating func createItem() {
+        item.contents = image.CGImage
+    }
 }
 
 struct GPSLocation {
