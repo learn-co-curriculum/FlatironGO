1c7cf85b9c93f285ac7855c2bef87c16985826fc
JimCampagno <jimcampagno@gmail.com>
2016-08-18 13:55:11 -0400

    Create a working challenge lab

--- FlatironGo/MapViewController.swift
+++ FlatironGo/MapViewController.swift
@@ -14,101 +14,47 @@ import GeoFire
 
 final class MapViewController: UIViewController  {
     
-    var locationManager = CLLocationManager()
-    var userStartLocation = CLLocation()
-    var treasureLocations: [String: GPSLocation] = [:]
-    var treasures: [(String, Treasure)] = []
     var annotations: [String: Treasure] = [:]
     var mapView: MGLMapView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupMapView()
-        setupCurrentLocation()
         setCenterCoordinateOnMapView()
-        getTreasuresFor(userStartLocation) { _ in }
+        generateDummyData()
     }
     
 }
 
-// MARK: - Treasure Methods
+// MARK: - Dummy Data
 extension MapViewController {
     
-    typealias ResponseDictionary = [String: AnyObject]
-    
-    private func getTreasuresFor(location: CLLocation, completion: (Bool) -> ()) {
-        let geoQuery = setupGeoQueryWithLocation(location)
-    
-        geoQuery.observeEventType(.KeyEntered) { [unowned self] key, location in
-            guard let geoKey = key,
-                geoLocation = location
-                else { print("No Key and/or No Location"); completion(false); return }
-            
-            let treasureLocation = self.generateLatAndLongFromLocation(geoLocation)
-            
-            self.treasureLocations[geoKey] = (GPSLocation(latitude: treasureLocation.lat, longitude: treasureLocation.long))
-            
-            self.getTreasureProfileFor(geoKey) { [unowned self] result in
-                if result { self.createAnnotations() }
-                completion(result)
-            }
+    private func generateDummyData() {
+        // let buzzLocation = GPSLocation(latitude: 40.7032775878906, longitude: -74.0170288085938)
+        let bullLocation = GPSLocation(latitude: 40.7043342590332, longitude: -74.0139770507812)
+        let funnyLocation = GPSLocation(latitude: 40.7052803039551, longitude: -74.0140228271484)
+        let nyseLocation = GPSLocation(latitude: 40.7056159973145, longitude: -74.0134048461914)
+        let polarLocation = GPSLocation(latitude: 40.7068748474121, longitude: -74.0112686157227)
+        
+        // let buzz = Treasure(location: buzzLocation, name: "Buzz Lightyear", imageURLString: "")
+        let bull = Treasure(location: bullLocation, name: "Charging Bull", imageURLString: "")
+        let funny = Treasure(location: funnyLocation, name: "Not Snorlax", imageURLString: "")
+        let nyse = Treasure(location: nyseLocation, name: "NYSE", imageURLString: "")
+        let polar = Treasure(location: polarLocation, name: "Hairy Harry", imageURLString: "")
+        
+        // buzz.image = UIImage(imageLiteral: "BuzzLightyear")
+        bull.image = UIImage(imageLiteral: "ChargingBull")
+        funny.image = UIImage(imageLiteral: "FunnyPhoto")
+        nyse.image = UIImage(imageLiteral: "NYSE")
+        polar.image = UIImage(imageLiteral: "PolarBear")
+        
+        let treasureObjects = [bull, funny, nyse, polar]
+        
+        for treasure in treasureObjects {
+            treasure.createItem()
+            generateAnnotationWithTreasure(treasure)
         }
-    }
-    
-    private func generateLatAndLongFromLocation(location: CLLocation) -> (lat: Float, long: Float) {
-        return (Float((location.coordinate.latitude)), Float((location.coordinate.longitude)))
-    }
-    
-    private func setupGeoQueryWithLocation(location: CLLocation) -> GFCircleQuery {
-        let geofireRef = FIRDatabase.database().referenceWithPath(FIRReferencePath.treasureLocations)
-        let geoFire = GeoFire(firebaseRef: geofireRef)
-        let geoQuery = geoFire.queryAtLocation(location, withRadius: 10.0)
-        return geoQuery
         
-    }
-    
-    private func getTreasureProfileFor(key: String, completion: (Bool) -> ()) {
-        let profileRef = FIRDatabase.database().referenceWithPath(FIRReferencePath.treasureProfiles + "/" + key)
-        
-        profileRef.observeEventType(FIRDataEventType.Value, withBlock: { [unowned self] snapshot in
-            guard let profile = snapshot.value as? ResponseDictionary,
-                treasureLocation = self.treasureLocations[snapshot.key]
-                else { print("Unable to produce snapshot value or key"); completion(false); return }
-            
-            self.saveTreasureLocally(withResponse: profile, key: snapshot.key, andLocation: treasureLocation)
-            completion(true)
-            })
-    }
-    
-    private func saveTreasureLocally(withResponse response: ResponseDictionary, key: String, andLocation location: GPSLocation) {
-        let name = response["name"] as? String ?? ""
-        let imageURL = response["imageURL"] as? String ?? ""
-        let treasure = Treasure(location: location, name: name, imageURLString: imageURL)
-        let newTreasure = (key, treasure)
-        treasures.append(newTreasure)
-        treasure.makeImage { _ in }
-    }
-    
-}
-
-// MARK: - Current Location Methods
-extension MapViewController: CLLocationManagerDelegate {
-    
-    func getUserLocation() -> CLLocation? {
-        locationManager.delegate = self
-        locationManager.desiredAccuracy = kCLLocationAccuracyBest
-        locationManager.requestWhenInUseAuthorization()
-        locationManager.startMonitoringSignificantLocationChanges()
-        
-        let weHaveAuthorization = (CLLocationManager.authorizationStatus() == CLAuthorizationStatus.AuthorizedWhenInUse || CLLocationManager.authorizationStatus() == CLAuthorizationStatus.AuthorizedAlways)
-        
-        if weHaveAuthorization { return locationManager.location } else { return nil }
-    }
-    
-    private func setupCurrentLocation() {
-        if let location = getUserLocation() {
-            userStartLocation = location
-        }
     }
     
 }
@@ -132,7 +78,10 @@ extension MapViewController {
     }
     
     private func setCenterCoordinateOnMapView() {
-        mapView.setCenterCoordinate(userStartLocation.coordinate, zoomLevel: 15, direction: 150, animated: false)
+        let lat: CLLocationDegrees = 40.705697302800182
+        let lng: CLLocationDegrees = -74.014699650804047
+        let downtownManhattan = CLLocationCoordinate2D(latitude: lat, longitude: lng)
+        mapView.setCenterCoordinate(downtownManhattan, zoomLevel: 15, direction: 25.0, animated: false)
     }
     
 }
@@ -186,8 +135,9 @@ extension MapViewController: MGLMapViewDelegate {
     }
     
     func mapViewDidFinishLoadingMap(mapView: MGLMapView) {
-        let camera = MGLMapCamera(lookingAtCenterCoordinate: mapView.centerCoordinate, fromDistance: 200, pitch: 60, heading: 180)
+        let camera = MGLMapCamera(lookingAtCenterCoordinate: mapView.centerCoordinate, fromDistance: 200, pitch: 60, heading: 0)
         mapView.setCamera(camera, withDuration: 2, animationTimingFunction: CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut))
+        mapView.resetNorth()
     }
     
 }
@@ -195,21 +145,16 @@ extension MapViewController: MGLMapViewDelegate {
 // MARK: - Annotation Methods
 extension MapViewController {
     
-    private func createAnnotations() {
-        guard let (_, treasure) = treasures.last else { print("No last treasure"); return }
-        generateAnnotationWithTreasure(treasure)
-    }
-    
     private func generateAnnotationWithTreasure(treasure: Treasure) {
         let newAnnotation = MGLPointAnnotation()
         let lat = Double(treasure.location.latitude)
         let long = Double(treasure.location.longitude)
         newAnnotation.coordinate = CLLocationCoordinate2D(latitude: lat, longitude: long)
         newAnnotation.title = treasure.name
-        mapView.addAnnotation(newAnnotation)
-        
         let key = String(newAnnotation.coordinate.latitude) + String(newAnnotation.coordinate.longitude)
         annotations[key] = treasure
+        mapView.addAnnotation(newAnnotation)
+        
     }
     
 }
@@ -219,17 +164,9 @@ extension MapViewController {
     
     private func handleTapOfAnnotationView(annotationView: MGLAnnotationView) {
         if let annotation = annotationView as? TreasureAnnotationView {
-            
-            // Providing a default treasure value to the annotations.treasure property if its nil (for w/e reason)
-            if annotation.treasure == nil {
-                annotation.treasure = Treasure(location: GPSLocation(latitude: 40.0, longitude: 40.0), name: "Charging Bull", imageURLString: Constants.bullImage)
-                annotation.treasure.makeImage() { [unowned self] success in
-                    self.performSegueWithIdentifier("TreasureSegue", sender: annotationView)
-                }
-            } else {
-                performSegueWithIdentifier("TreasureSegue", sender: annotationView)
-            }
+            performSegueWithIdentifier("TreasureSegue", sender: annotation)
         }
+        
     }
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
